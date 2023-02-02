3659bee73c76219398ad064e09edcdae85d97c33
Gani Georgiev <gani.georgiev@gmail.com>
2023-02-02 10:14:57 +0200

    fixed field example typo

--- src/routes/(app)/docs/api-records/List.svelte
+++ src/routes/(app)/docs/api-records/List.svelte
@@ -87,7 +87,7 @@
 
             // fetch a paginated records list
             const resultList = await pb.collection('posts').getList(1, 50, {
-                filter: 'created >= "2022-01-01 00:00:00" && someFiled1 != someField2',
+                filter: 'created >= "2022-01-01 00:00:00" && someField1 != someField2',
             });
 
             // you can also fetch all records at once via getFullList
@@ -111,7 +111,7 @@
             final resultList = await pb.collection('posts').getList(
               page: 1,
               perPage: 50,
-              filter: 'created >= "2022-01-01 00:00:00" && someFiled1 != someField2',
+              filter: 'created >= "2022-01-01 00:00:00" && someField1 != someField2',
             );
 
             // you can also fetch all records at once via getFullList
