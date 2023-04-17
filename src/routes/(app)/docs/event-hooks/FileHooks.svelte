50da4d48155560cbe805f3324eb47e2400ec09e3
Gani Georgiev <gani.georgiev@gmail.com>
2023-04-17 22:15:40 +0300

    added OnFileBeforeTokenRequest and OnFileAfterTokenRequest docs

--- src/routes/(app)/docs/event-hooks/FileHooks.svelte
+++ src/routes/(app)/docs/event-hooks/FileHooks.svelte
@@ -41,4 +41,76 @@
             `}
         />
     </Accordion>
+
+    <Accordion single>
+        <svelte:fragment slot="header">
+            <div class="flex txt-bold txt-select">OnFileBeforeTokenRequest</div>
+        </svelte:fragment>
+        <p>Triggered before each file token API request.</p>
+        <p>
+            If no token or model was submitted, <code>e.Model</code> and <code>e.Token</code> will be empty, allowing
+            you to implement your own custom model file auth implementation.
+        </p>
+        <CodeBlock
+            language="go"
+            content={`
+                        package main
+
+                        import (
+                            "log"
+
+                            "github.com/pocketbase/pocketbase"
+                            "github.com/pocketbase/pocketbase/core"
+                        )
+
+                        func main() {
+                            app := pocketbase.New()
+
+                            app.OnFileBeforeTokenRequest().Add(func(e *core.FileTokenEvent) error {
+                                log.Println(e.Model)
+                                log.Println(e.Token)
+                                return nil
+                            })
+
+                            if err := app.Start(); err != nil {
+                                log.Fatal(err)
+                            }
+                        }
+                    `}
+        />
+    </Accordion>
+
+    <Accordion single>
+        <svelte:fragment slot="header">
+            <div class="flex txt-bold txt-select">OnFileAfterTokenRequest</div>
+        </svelte:fragment>
+        <p>Triggered after each successful file token API request.</p>
+        <CodeBlock
+            language="go"
+            content={`
+                        package main
+
+                        import (
+                            "log"
+
+                            "github.com/pocketbase/pocketbase"
+                            "github.com/pocketbase/pocketbase/core"
+                        )
+
+                        func main() {
+                            app := pocketbase.New()
+
+                            app.OnFileAfterTokenRequest().Add(func(e *core.FileTokenEvent) error {
+                                log.Println(e.Model)
+                                log.Println(e.Token)
+                                return nil
+                            })
+
+                            if err := app.Start(); err != nil {
+                                log.Fatal(err)
+                            }
+                        }
+                    `}
+        />
+    </Accordion>
 </div>
