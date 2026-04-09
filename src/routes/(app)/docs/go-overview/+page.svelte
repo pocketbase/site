0a5f331abd1c2783d5cb699c69ec256f67938f66
Gani Georgiev <gani.georgiev@gmail.com>
2026-04-09 22:21:01 +0300

    updated ncruces driver example

--- src/routes/(app)/docs/go-overview/+page.svelte
+++ src/routes/(app)/docs/go-overview/+page.svelte
@@ -228,7 +228,6 @@
                     "github.com/pocketbase/pocketbase"
 
                     _ "github.com/ncruces/go-sqlite3/driver"
-                    _ "github.com/ncruces/go-sqlite3/embed"
                 )
 
                 func main() {
